76e59794053bdc9cc35cf179749b76828b2a0dfe
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-07-02 22:52:39 +0530

    controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -34,6 +34,23 @@ exports.create = (req,res)=>{
 //retrive and return ll users
 
 exports.find = (req,res)=>{
+
+  if(req.query.id){
+    const id = req.query.id;
+
+    UserDb.findById(id)
+    .then(data=>{
+      if(!data){
+        res.status(404).send({message:`Not found user with id ${id}`})
+      }else{
+        res.send(data);
+      }
+    })
+    .catch(err=>{
+      res.status(500).send({message: `Error retrieving user with id ${id}` })
+    })
+  }
+
   UserDb.find()
   .then(user =>{
     res.send(user)
